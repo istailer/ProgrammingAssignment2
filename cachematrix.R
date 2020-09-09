9b53ea7c736700028e0d7ebee2475017539ed3ba
Istailer Souza <istailer@hotmail.com>
2020-09-09 18:15:09 -0300

    update my assignment week 3, R programming Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My Assignment Week3: Caching the Inverse of a Matrix
+## github 'istailer'. Thank's four your time
 
-## Write a short comment describing this function
+### Sorry, some comments are in Portuguese so I can understand in the future.
 
-makeCacheMatrix <- function(x = matrix()) {
+## Function to store the inverse of a matrix in the cache
+
+makeCacheMatrix <- function(x = matrix()) {                     # valor padrao de x = matrix
+        invM <- NULL                                            # invM com valor NULO
+        set <- function(y) {                                    # funcao para 'setar' novo valor
+                x <<- y
+                invM <<- NULL
+        }
+        get <- function() x                                     # funcao para retornar o valor da matriz
+        setinv <- function(invArg) invM <<- invArg              # funcao para atribui valor inverso
+        getinv <- function() invM                               # funcao para obter o valor inverso
+        list(set = set,                                         # criar lista para usar 'names' $
+             get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of the Matrix (if necessary) of makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invM <- x$getinv()                                      # obtem o valor de x inverso
+        if(!is.null(invM)){                                     # funcao para verificar se x não é NULO e retorno x inverso
+                message("getting cached data")
+                return(invM)
+        }
+        data <- x$get()                                         # 'data' obtem o valor de x
+        invM <- solve(data,...)                                 # 'invM' atribui o valor da funcao 'solve' - estudar essa funcao
+        x$setinv(invM)                                          # novo valor e "setado" para x
+        invM                                                    # mostra o valor de x (matriz)
 }
